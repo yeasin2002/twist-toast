7660402096ff0c7a3839c8928f18bcc3ae088bb2
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-26 00:04:11 +0600

    feat(react): add createToast function for managing toast notifications

---
+++ packages/react/src/index.ts
@@ -0,0 +1,16 @@
+export type {
+  CreateToastOptions,
+  ToastCallOptions,
+  ToastDeduplication,
+  ToastPosition,
+  ToastRecord,
+  ToastRole,
+} from "@twist-toast/core";
+export { createToast } from "./create-toast";
+export { ToastProvider } from "./ToastProvider";
+export type {
+  ToastComponentProps,
+  ToastInstance,
+  ToastProviderProps,
+  ToastVariant,
+} from "./types";
