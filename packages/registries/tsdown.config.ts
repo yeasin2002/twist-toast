802f8125f210cc894289a1b14b82847eab7081b4
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-28 09:10:57 +0600

    refactor: removed collections, moved collections to component registries

--- packages/registries/tsdown.config.ts
+++ packages/registries/tsdown.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   platform: "neutral",
   dts: true,
-  entry: ["./src/collections/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
+  entry: ["./collections/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
 });
