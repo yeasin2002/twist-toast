b2df0ccc04985c41b6bfa6c22a04168f34cd3606
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-02-28 09:56:02 +0600

    refactor(www): removed all collections-list from www and using registry

--- apps/www/src/components/collections-list/hot-toast/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./error";
-export * from "./hot-toaster";
-export * from "./info";
-export * from "./loading";
-export * from "./success";
-export * from "./types";
-export * from "./warning";
--- apps/www/src/components/collections-list/hot-toast/types.ts
+++
@@ -1,4 +0,0 @@
-export type HotToastItemProps = {
-  message?: string;
-  className?: string;
-};
